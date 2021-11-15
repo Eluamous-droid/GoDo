4038e8257fbf6a5c7c5eedef202a9d713957d3e9
Mikkel Hansen <themik1337@gmail.com>
2021-11-15 22:12:48 +0100

    Added command to list file and removed need for flags

--- todo/tools/fileManager.go
+++ todo/tools/fileManager.go
@@ -1,25 +1,38 @@
 package tools
 
 import (
+	"fmt"
 	"os"
 	"path"
 )
 
+var filePath string = os.ExpandEnv("$HOME/.todocli/")
+var fileName string = "todos.txt"
+
 func SaveToFile(input string){
-	path := "$HOME/.todocli/"
-	ensureBaseDir(path)
-	f,err := os.Create(os.ExpandEnv(path+"todos.txt"))
+	
+	ensureBaseDir(filePath)
+	f,err := os.OpenFile(filePath+fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	check(err)
 	defer f.Close()
 
-	_,err2 := f.WriteString(input)
+	_,err2 := f.WriteString(input + " \n")
 
 	check(err2)
 	f.Sync()
 }
 
+func ReadTodos(){
+	ensureBaseDir(filePath)
+	f,err := os.ReadFile(filePath+fileName)
+	check(err)
+
+	fmt.Print(string(f))
+
+}
+
 func ensureBaseDir(fpath string) error {
-    baseDir := path.Dir(os.ExpandEnv(fpath))
+    baseDir := path.Dir(filePath)
     info, err := os.Stat(baseDir)
     if err == nil && info.IsDir() {
        return nil
--- todo/tools/inputReader.go
+++ todo/tools/inputReader.go
@@ -2,22 +2,31 @@ package tools
 
 import (
 	"flag"
-	"fmt"
 	"os"
 )
 
 
 func ReadInput(){
-	textPtr := flag.String("text", "", "Text to parse. (Required)")
-    metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};. (Required)")
-    uniquePtr := flag.Bool("unique", false, "Measure unique values of a metric.")
+
+    args := os.Args[1:]
+	//todoPtr := flag.String("ToDo", "", "Text to save.")
+    //listCommand := flag.NewFlagSet("list", flag.ExitOnError)
+
+    switch args[0] {
+    case "list":
+        ReadTodos()
+    default:
+    SaveToFile(args[0])
+    }
+
     flag.Parse()
 
-    if *textPtr == "" {
-        flag.PrintDefaults()
-        os.Exit(1)
+    //if *todoPtr == "" {
+      //  flag.PrintDefaults()
+        //os.Exit(1)
 		
-    }
-	SaveToFile(*textPtr)
-    fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
+    //}
+    
+    //SaveToFile(args[0])
+	
 }
